ddf25ceb2cb160671da49c6d6d7bbe2d1a3efa51
Vladimir <papa1963@hotmail.de>
2024-07-13 18:41:23 +0200

    inintial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        CurrencyExchange currencyExchange = new CurrencyExchange();
+        ExchangeHistory exchangeHistory = new ExchangeHistory();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите сумму обмена");
+            double amount = scanner.nextDouble();
 
-    }
-}
+            System.out.println("Введите валюту которую хотите обменять");
+            String fromCurrency = scanner.next();
 
+            System.out.println("Введите валюту которую хотите приобрести");
+            String toCurrency = scanner.next();
 
+            double result = currencyExchange.exchange(amount, fromCurrency, toCurrency);
+            exchangeHistory.addRecord(amount, fromCurrency, result, toCurrency);
+
+            exchangeHistory.showHistory();
+        }}}
